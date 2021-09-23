2e95522c7ef7d9878bf39a8f7a959855138ff968
EmmanuelChangole <manuchangole@gmail.com>
2021-09-23 22:14:32 +0300

    material design

--- app/src/main/java/com/example/mentalhealthapp/ItemFragment.kt
+++ app/src/main/java/com/example/mentalhealthapp/ItemFragment.kt
@@ -1,12 +1,12 @@
 package com.example.mentalhealthapp
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.example.mentalhealthapp.data.model.Disorder
 import kotlinx.android.synthetic.main.fragment_item_list.*
 
@@ -14,7 +14,7 @@ import kotlinx.android.synthetic.main.fragment_item_list.*
  * A fragment representing a list of Items.
  */
 class ItemFragment : Fragment() {
-    private val  nicCageMovies = listOf(
+    private val  disorder = listOf(
         Disorder("Anxiety","How to deal with Anxiety",R.drawable.ic_anxiety),
         Disorder("Apathy","How to deal with Apathy",R.drawable.ic_apathy),
         Disorder("Envy","How to deal with Envy",R.drawable.ic_envy),
@@ -30,18 +30,34 @@ class ItemFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
         retainInstance = true
     }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? =
-        inflater.inflate(R.layout.fragment_item_list, container, false)
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater)
+    {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.main_menu,menu)
 
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        //navigate to the Fragment that has the same id as the selected menu item
+        return NavigationUI.
+        onNavDestinationSelected(item,requireView().findNavController())
+                || super.onOptionsItemSelected(item)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View?
+    {
+      return   inflater.inflate(R.layout.fragment_item_list, container, false)
+    }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        (activity as AppCompatActivity?)!!.supportActionBar!!.hide()
+        (activity as AppCompatActivity?)!!.supportActionBar!!.show()
         list_recycler_view.apply{
             layoutManager = LinearLayoutManager(activity)
-            adapter = MyItemRecyclerViewAdapter(nicCageMovies)
+            adapter = MyItemRecyclerViewAdapter(disorder)
         }
 
 
