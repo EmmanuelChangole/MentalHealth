90bd78a9a18d470f0fbcc8dabc44b3e56c9af547
EmmanuelChangole <manuchangole@gmail.com>
2021-10-07 08:34:38 +0300

    Notifications

---
+++ app/src/main/java/com/example/mentalhealthapp/receiver/AlarmReceiver.kt
@@ -0,0 +1,29 @@
+
+package com.example.mentalhealthapp.receiver
+
+import android.app.NotificationManager
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.widget.Toast
+import androidx.core.content.ContextCompat
+import com.example.mentalhealthapp.R
+import com.example.mentalhealthapp.utils.sendNotification
+
+class AlarmReceiver: BroadcastReceiver() {
+
+    override fun onReceive(context: Context, intent: Intent) {
+
+        val notificationManager = ContextCompat.getSystemService(
+            context,
+            NotificationManager::class.java
+        ) as NotificationManager
+
+        notificationManager.sendNotification(
+            context.getText(R.string.ready).toString(),
+            context
+        )
+
+    }
+
+}
---
+++ app/src/main/java/com/example/mentalhealthapp/utils/NotificationUtil.kt
@@ -0,0 +1,55 @@
+package com.example.mentalhealthapp.utils
+
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.graphics.BitmapFactory
+import androidx.core.app.NotificationCompat
+import com.example.mentalhealthapp.MainActivity
+import com.example.mentalhealthapp.R
+import com.example.mentalhealthapp.receiver.SnoozeReceiver
+
+// Notification ID.
+private val NOTIFICATION_ID = 0
+private val REQUEST_CODE = 0
+private val FLAGS = 0
+
+fun NotificationManager.sendNotification(messageBody: String, applicationContext: Context)
+{
+    val contentIntent = Intent(applicationContext, MainActivity::class.java)
+    val contentPendingIntent = PendingIntent.getActivity(
+        applicationContext,
+        NOTIFICATION_ID,
+        contentIntent,
+        PendingIntent.FLAG_UPDATE_CURRENT
+    )
+
+    val iconImage = BitmapFactory.decodeResource(
+        applicationContext.resources,
+        R.mipmap.ic_launcher
+    )
+    val bigPicStyle = NotificationCompat.BigPictureStyle()
+        .bigPicture(iconImage)
+        .bigLargeIcon(null)
+    val snoozeIntent = Intent(applicationContext, SnoozeReceiver::class.java)
+    val snoozePendingIntent: PendingIntent = PendingIntent.getBroadcast(
+        applicationContext,
+        REQUEST_CODE,
+        snoozeIntent,
+        FLAGS)
+    // Build the notification
+    val builder = NotificationCompat.Builder(
+        applicationContext,
+        applicationContext.getString(R.string.notification_channel_id)
+    ).setSmallIcon(R.mipmap.ic_launcher)
+        .setContentTitle(applicationContext
+            .getString(R.string.notification_title))
+        .setContentText(messageBody)
+        .setContentIntent(contentPendingIntent)
+        .setAutoCancel(true)
+        .setStyle(bigPicStyle)
+        .setLargeIcon(iconImage)
+        .setPriority(NotificationCompat.PRIORITY_HIGH)
+        notify(NOTIFICATION_ID, builder.build())
+}
